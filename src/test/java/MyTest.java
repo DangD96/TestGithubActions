be7e49781d9f0fa99d81df5eaf89a26bb620f113
DangD96 <35606061+DangD96@user.noreply.github.com>
2025-01-19 23:30:48 -0600

    add support for edge and firefox

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -16,11 +20,34 @@ public class MyTest {
     @BeforeTest
     public void setUp() throws MalformedURLException {
         String env = System.getProperty("env");
-        ChromeOptions options = new ChromeOptions();
-        if ("local".equals(env)) {driver = new ChromeDriver(options);}
-        else {
-            // The name of the Docker selenium hub service is used as the hostname in the URL to access the Selenium Grid Hub
-            driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+        String browser = System.getProperty("browser");
+        switch (browser) {
+            case "Chrome":
+                ChromeOptions options = new ChromeOptions();
+                if ("local".equals(env)) {driver = new ChromeDriver(options);}
+                else {
+                    // The name of the Docker selenium hub service is used as the hostname in the URL to access the Selenium Grid Hub
+                    driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options);
+                }
+                break;
+            case "Edge":
+                EdgeOptions options2 = new EdgeOptions();
+                if ("local".equals(env)) {driver = new EdgeDriver(options2);}
+                else {
+                    // The name of the Docker selenium hub service is used as the hostname in the URL to access the Selenium Grid Hub
+                    driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options2);
+                }
+                break;
+            case "Firefox":
+                FirefoxOptions options3 = new FirefoxOptions();
+                if ("local".equals(env)) {driver = new FirefoxDriver(options3);}
+                else {
+                    // The name of the Docker selenium hub service is used as the hostname in the URL to access the Selenium Grid Hub
+                    driver = new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), options3);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
         }
         driver.manage().window().maximize();
     }
@@ -34,6 +61,6 @@ public class MyTest {
     @AfterTest
     public void tearDown() {
         driver.quit();
-        System.out.println("DID IT WORK");
+        System.out.println("I JUST RAN ON: " + System.getProperty("browser").toUpperCase());
     }
 }

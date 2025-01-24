d7b5df646fe6188f2940ab5267522026cf2394ad
DangD96 <35606061+DangD96@user.noreply.github.com>
2025-01-23 19:34:53 -0600

    remove

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,7 +1,5 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
